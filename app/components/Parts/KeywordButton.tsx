9a03178141dbe7435e38dc2bd157f98c1cb20e69
mi3765 <chips1225@outlook.jp>
2024-01-02 23:28:50 +0900

    update keyword-button and icons

--- app/components/Parts/KeywordButton.tsx
+++ app/components/Parts/KeywordButton.tsx
@@ -1,12 +1,47 @@
 "use client";
 import React from "react";
 import { Button } from "@material-tailwind/react";
+import RestaurantOutlinedIcon from "@mui/icons-material/RestaurantOutlined";
+import RamenDiningOutlinedIcon from "@mui/icons-material/RamenDiningOutlined";
+import FreeBreakfastOutlinedIcon from "@mui/icons-material/FreeBreakfastOutlined";
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
 
-const KeywordButton = () => {
+interface KeywordButtonProps {
+	keyword: string;
+	index: number;
+}
+
+const KeywordButton: React.FC<KeywordButtonProps> = ({ keyword, index }) => {
+	let selectedIcon;
+	if (index === 0) {
+		selectedIcon = (
+			<RestaurantOutlinedIcon fontSize="small" className="text-green-500" />
+		);
+	} else if (index === 1) {
+		selectedIcon = (
+			<RamenDiningOutlinedIcon fontSize="small" className="text-green-500" />
+		);
+	} else if (index === 2) {
+		selectedIcon = (
+			<FreeBreakfastOutlinedIcon fontSize="small" className="text-green-500" />
+		);
+	} else {
+		selectedIcon = (
+			<StoreOutlinedIcon fontSize="small" className="text-green-500" />
+		);
+	}
+
 	return (
 		<div>
-			<Button variant="gradient" placeholder={undefined}>
-				gradient
+			<Button
+				variant="outlined"
+				color="green"
+				className="rounded-full flex items-center gap-2"
+				placeholder={undefined}
+				size="sm"
+			>
+				{selectedIcon}
+				{keyword}
 			</Button>
 		</div>
 	);
