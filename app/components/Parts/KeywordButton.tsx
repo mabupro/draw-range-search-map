3034b7b3f14cd9e6a6d50fc5813a22266724f68a
mi3765 <chips1225@outlook.jp>
2024-01-02 22:22:13 +0900

    create keyword-button

---
+++ app/components/Parts/KeywordButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { Button } from "@material-tailwind/react";
+
+const KeywordButton = () => {
+	return (
+		<div>
+			<Button variant="gradient" placeholder={undefined}>
+				gradient
+			</Button>
+		</div>
+	);
+};
+
+export default KeywordButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { DrawLine } from "./components/Map/DrawLine";
-import { ShowMap } from "./components/Map/ShowMap";
+// import { ShowMap } from "./components/Map/ShowMap";
+import KeywordButton from "./components/Parts/KeywordButton";
 
 export default function Home() {
 	return (
 		<>
-			<ShowMap />
-			<DrawLine />
+			<div>
+				{/* <ShowMap /> */}
+				<DrawLine />
+				<KeywordButton />
+			</div>
 		</>
 	);
 }
