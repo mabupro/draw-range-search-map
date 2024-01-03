ac0ade4ae5d2c6fb08ea69fdd5800424e11f06bd
mi3765 <chips1225@outlook.jp>
2024-01-03 22:25:04 +0900

    location success

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import { DrawLine } from "./components/Map/DrawLine";
 import { ShowMap } from "./components/Map/ShowMap";
 import KeywordButton from "./components/Parts/KeywordButton";
+import { LocateButton } from "./components/Parts/LocateButton";
+import { useState } from "react";
 
 const array: string[] = ["レストラン", "ラーメン", "コーヒー", "コンビニ"];
 
 export default function Home() {
+	const [currentLocation, setCurrentLocation] = useState({
+		lat: 36,
+		lng: 139,
+	});
+	const handleLocationUpdate = (currentLocation: {
+		lat: number;
+		lng: number;
+	}) => {
+		setCurrentLocation(currentLocation);
+		console.log(currentLocation.lat, currentLocation.lng);
+	};
+
 	return (
 		<>
 			<div className="relative">
@@ -13,6 +28,7 @@ export default function Home() {
 					<DrawLine />
 				</div>
 				<div className="absolute top-1/2 right-0 transform -translate-y-1/2 flex flex-col gap-2">
+					<LocateButton onLocate={handleLocationUpdate} />
 					{array.map((val, index) => (
 						<KeywordButton key={index} keyword={val} index={index} />
 					))}
