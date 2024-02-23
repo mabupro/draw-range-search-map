2eedc8c2f8b516478d2d9fdc1b19fbd0923f19f0
mabupro <mabupro787@gmail.com>
2024-02-23 13:33:41 +0900

    vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,7 +27,7 @@ export default function RootLayout({
           />
         )}
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<SpeedInsights /><Analytics /></body>
     </html>
   );
 }
